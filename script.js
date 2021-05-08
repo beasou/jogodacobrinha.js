f93c5113e0185a877cbdadcf643ba8da1b572c28
beasou <beatrizreginasouza1@gmail.com>
2021-05-08 17:44:41 -0300

    Finalizar Jogo se a cobrinha chocar contra ela mesma

--- script.js
+++ script.js
@@ -48,7 +48,16 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
-    
+
+    //definir se a cobrinha vai se chocar contra o propio corpo
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            //parar a função criar jogo
+            clearInterval(jogo);
+            alert('Game Ovar:(');
+        }
+    }
+
     //chamando as funções
     criarBG(); 
     criarCobrinha();
